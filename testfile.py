412a756874f128f1dab38d56e026e286b2cce318
marvin merida <penguinonice@marvins-MacBook-Pro.local>
2025-06-17 11:03:37 -0700

    completed lab 2

---
+++ testfile.py
@@ -0,0 +1,20 @@
+# lab 1
+
+## step 1
+
+name = "Marvin"
+
+## step 2
+nickname = "Starvin"
+
+## step 3
+age = 15
+
+## step 4
+used_python = False
+
+## step 5
+hobbies = ["gaming", "sleep", "traveling to my bed"]
+
+## step 6
+favorite_thing = {"my phone", "my tv"}
