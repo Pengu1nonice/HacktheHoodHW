32a850e4729a7b4f6a482056bd9c7ff0403a7a4b
marvin merida <penguinonice@marvins-MacBook-Pro.local>
2025-06-30 13:27:23 -0700

    lab4.py

---
+++ Demo.py
@@ -0,0 +1,31 @@
+#checking = "yes"
+
+#while checking == "yes":
+    #print("what is your age: ")
+   # user_input = input()
+   # if int(user_input) >=18:
+    # print("yes you can vote")
+    #else:
+       #print("you can vote")
+#print("would you like to check another age?")
+#user_input2 = input()
+#checking = user_input2
+
+#list1 = [3, -1, 0, 6, 14,]
+
+#for x in list1:
+    #if x > 0:
+        #print("value is positive")
+    #elif x < 0:
+        #print("value is negative")
+    #else: 
+
+inventory = ["tnt", "glass", "netherite", "waxed lightly weathered chiseled copper stair"]
+list_length = len(inventory)
+
+for i in range (list_length):
+    if i == "waxed lightly weathered chiseled copper stair":
+        print("why do you have this in your inventory")
+    elif i == "tnt":
+     print("go bomb jhhny house")
+     
